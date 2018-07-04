7590db038771482c54f31bd6b930d9404df8079b
Diego J <diegojromerolopez@gmail.com>
2018-07-05 00:55:42 +0200

    Docs in aggregation

--- lib/babik/query/aggregation.rb
+++ lib/babik/query/aggregation.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+# Common module for Babik library
 module Babik
 
+  # Abstract aggregation. Do not use
   class AbstractAggregation
     SQL_OPERATION = 'OVERWRITE IN CHILDREN CLASSES'
     attr_reader :model, :selection, :field_name
@@ -32,18 +34,22 @@ module Babik
     operation_class.new(aggregation_path)
   end
 
+  # Average operation. Compute the mean of a set of values.
   class Avg < AbstractAggregation
     SQL_OPERATION = 'AVG'
   end
 
+  # Max operation. Compute the maximum of a set of values.
   class Max < AbstractAggregation
     SQL_OPERATION = 'MAX'
   end
 
+  # Min operation. Compute the minimum of a set of values.
   class Min < AbstractAggregation
     SQL_OPERATION = 'MIN'
   end
 
+  # Sum operation. Compute the sum of a set of values.
   class Sum < AbstractAggregation
     SQL_OPERATION = 'SUM'
   end
