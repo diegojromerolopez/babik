8aa1428c6f49fc2cfa90cbd86aab1928ce95d5fb
Diego J <diegojromerolopez@gmail.com>
2018-07-22 00:23:35 +0200

    More documentation

--- lib/babik/queryset/lib/field.rb
+++ lib/babik/queryset/lib/field.rb
@@ -7,11 +7,17 @@ module Babik
     # abstracts the concept of table field according to some useful conversions
     class Field
 
+      # Create an actual field for a model.
+      # @param model [ActiveRecord::Base] model this field belongs to.
+      # @param field [String] field model that could need the conversion.
       def initialize(model, field)
         @model = model
         @field = field
       end
 
+      # Check if the field requires some conversion and if that's the case, return the converted final field
+      # If the field is a name of an association, it will be converted to the foreign entity id
+      # @return [String] Actual name of the field that will be used in the SQL.
       def real_field
         # If the selected field is a local attribute return the condition as-is (that's the most usual case)
         is_local_attribute = @model.column_names.include?(@field.to_s)
