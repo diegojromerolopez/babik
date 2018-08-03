3aeb0f8480cd9469843959ff9932b3ed630199db
Diego J <diegojromerolopez@gmail.com>
2018-08-04 00:00:43 +0200

    Refactor Selection Operations

--- lib/babik/queryset/lib/selection/operation/base.rb
+++ lib/babik/queryset/lib/selection/operation/base.rb
@@ -45,24 +45,52 @@ module Babik
         def self.factory(field, operator, value)
           # Some operators can have a secondary operator, like the year lookup that can be followed by
           # a gt, lt, equal, etc. Check this case, and get it to prepare its passing to the operation.
+          raw_main_operator, secondary_operator = self.initialize_operators(operator)
+          # The field, operator or value can change in some special cases, e.g. if operator is equals and the value
+          # is an array, the operator should be 'in' actually.
+          field, main_operator, value = self.special_cases(field, raw_main_operator, value)
+          # At last, initialize operation
+          self.initialize_operation(field, main_operator, secondary_operator, value)
+        end
+
+        # Inform if the operation has a operator
+        # @return [Boolean] True if the operation needs an operator, false otherwise.
+        def self.operator?
+          self.const_defined?('HAS_OPERATOR') && self.const_get('HAS_OPERATOR')
+        end
+
+        # Initialize the operators (both main and secondary)
+        # When the operator is an Array, it means it actually is two different operators. The first one will be applied
+        # to the main operation, and the second one, to the lookup.
+        # e.g. selector 'created_at__time__gt' contains two operators, 'time' and 'gt'.
+        # @return [Array<String>] Array with both operations.
+        #         First element will be the main operation, the second one will be
+        #         the secondary operation. If there is no secondary operation, the second item will be nil.
+        def self.initialize_operators(operator)
           secondary_operator = nil
           if operator.class == Array
             secondary_operator = operator[1]
             operator = operator[0]
           end
-          # The field, operator or value can change in some special cases, e.g. if operator is equals and the value
-          # is an array, the operator should be 'in' actually.
-          final_field, final_operator, final_value = self.special_cases(field, operator, value)
-          final_operator_class_name = Babik::Selection::Operation::CORRESPONDENCE[final_operator.to_sym]
-          raise "Unknown lookup #{final_operator}" unless final_operator_class_name
-          final_operator_class = Object.const_get("Babik::Selection::Operation::#{final_operator_class_name}")
+          [operator, secondary_operator]
+        end
+
+        # Initialize the operation
+        # @param field [String] Field name that takes part in the selection operation.
+        # @param operator [Symbol] Operator name that defines which operation will be used.
+        # @param secondary_operator [Symbol] Some operations have a particular operation that's it.
+        # @param value [String, Integer] Value used in the selection.
+        # @return [Babik::Selection::Operation::Base] A SQL selection operation.
+        def self.initialize_operation(field, operator, secondary_operator, value)
+          operation_class_name = Babik::Selection::Operation::CORRESPONDENCE[operator.to_sym]
+          raise "Unknown lookup #{operator}" unless operation_class_name
+          operation_class = Object.const_get("Babik::Selection::Operation::#{operation_class_name}")
           # If there is a secondary operator, pass it to the operation
-          if secondary_operator ||
-            (final_operator_class.const_defined?('HAS_OPERATOR') && final_operator_class.const_get('HAS_OPERATOR'))
-            return final_operator_class.new(final_field, secondary_operator, final_value)
+          if secondary_operator || operation_class.operator?
+            return operation_class.new(field, secondary_operator, value)
           end
           # Otherwise, return the operation
-          final_operator_class.new(final_field, final_value)
+          operation_class.new(field, value)
         end
 
         # Special conversion of operations
